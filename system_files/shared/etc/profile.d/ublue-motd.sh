bb9b8c682fe7a27df542ccdad141317aff297339
Dylan M. Taylor <dylan@dylanmtaylor.com>
2026-06-25 14:12:25 -0400

    fix(motd): prevent double-execution and missing env.sh errors
    Add double-execution guard checking UBLUE_MOTD_SHOWN to profile.d/ublue-motd.sh and fish_greeting.fish so the motd is not run multiple times on each shell startup.
    
    Prevent fatal error by adding a check to ensure env.sh exists before trying to source it in ublue-motd.

--- system_files/shared/etc/profile.d/ublue-motd.sh
+++ system_files/shared/etc/profile.d/ublue-motd.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-[ ! -e "$HOME"/.config/no-show-user-motd ] && ublue-motd
+[ ! -e "$HOME"/.config/no-show-user-motd ] && [ "${UBLUE_MOTD_SHOWN:-0}" != "1" ] && { export UBLUE_MOTD_SHOWN=1; ublue-motd; }
--- system_files/shared/usr/share/fish/vendor_conf.d/fish_greeting.fish
+++ system_files/shared/usr/share/fish/vendor_conf.d/fish_greeting.fish
@@ -1,5 +1,8 @@
 function fish_greeting
     if test ! -e $HOME/.config/no-show-user-motd
-        ublue-motd
+        if test -z "$UBLUE_MOTD_SHOWN" -or "$UBLUE_MOTD_SHOWN" != "1"
+            set -gx UBLUE_MOTD_SHOWN 1
+            ublue-motd
+        end
     end
 end
